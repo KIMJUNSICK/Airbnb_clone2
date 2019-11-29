c238f1f7cb9850c60ed720ce709e3ff58ff68b41
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-29 14:08:30 +0900

    Model: finish user model

--- users/models.py
+++ users/models.py
@@ -4,4 +4,46 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OHTER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OHTER, "Other"),
+    )
+
+    LANGUAGE_KOREAN = "korean"
+    LANGUAGE_ENGLISH = "english"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_KRW = "krw"
+    CURRENCY_USD = "usd"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_USD, "USD"),
+    )
+
+    # You need to install python library called 'pillow' for processing image
+    # 'null=' in db, 'blank' in form
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=10, null=True, blank=True, choices=GENDER_CHOICES
+    )
+    bio = models.TextField(default="", null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, blank=True, null=True, max_length=7
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, blank=True, null=True, max_length=3
+    )
+    superhost = models.BooleanField(default=False)
