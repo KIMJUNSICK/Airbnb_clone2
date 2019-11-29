9e6dec99f188265aa0372bf905a0d2e7664a971d
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-29 21:56:17 +0900

    Admin: add Custom fieldset to UserAdmin's fieldset

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-
+# admin.ModelAdmin -> UserAdmin
+# Import & Use: this is the essential reason for developing project with Django
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    list_display = ("username", "email", "gender", "language")
-    list_filter = ("language", "currency", "superhost")
+    """ Custom  User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
