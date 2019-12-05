2b9bab986e1a08ce8d2c3d4afd186c702caaa2f6
KIMJUNSICK <jinaganda5@gmail.com>
2019-12-05 12:42:12 +0900

    Model: basic room datamodeling

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = ["django_countires"]
+
 PROJECT_APPS = ["users.apps.UsersConfig", "rooms.apps.RoomsConfig"]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
@@ -92,9 +94,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model Definition """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     # I don't want to this to db. because this is parts for extending other models
     # so write code like below
